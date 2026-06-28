377dbe7f1af5fa2e0774eff382822c4783f72f94
Aileen Morgan <amorgan101010@protonmail.com>
2026-06-27 21:22:03 -0400

    gemini sketch of the manager

---
+++ scripts/AudioManager.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+public partial class AudioManager : Node
+{
+    private AudioStreamPlayer _sfxPunch;
+
+    public override void _Ready()
+    {
+        // Set up your sound players here
+        _sfxPunch = new AudioStreamPlayer();
+        _sfxPunch.Stream = GD.Load<AudioStream>("res://samples/punch.wav");
+        AddChild(_sfxPunch);
+    }
+
+    public void PlayPunch()
+    {
+        _sfxPunch.Play();
+    }
+}
